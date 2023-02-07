c2111eb7ca08f6ce2fcbde893bb23731c1be0a43
nsunwoo <sunwoo.stats@gmail.com>
2023-02-07 16:22:35 +0900

    BOJ 1000: Add

---
+++ baekjoon/src/io_and_4ao/Add.java
@@ -0,0 +1,13 @@
+package io_and_4ao;
+
+import java.util.Scanner;
+
+public class Add {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a, b;
+    a = sc.nextInt();
+    b = sc.nextInt();
+    System.out.println(a + b);
+  }
+}
