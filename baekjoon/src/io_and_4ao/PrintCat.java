958247ea34594b075c553ced4c900ff9b44c8263
nsunwoo <sunwoo.stats@gmail.com>
2023-02-08 12:03:59 +0900

    BOJ 10171: PrintCat

---
+++ baekjoon/src/io_and_4ao/PrintCat.java
@@ -0,0 +1,16 @@
+// https://www.acmicpc.net/problem/10171
+
+package io_and_4ao;
+
+public class PrintCat {
+
+  public static void main(String[] args) {
+    
+    System.out.println("\\    /\\");
+    System.out.println(" )  ( ')");
+    System.out.println("(  /  )");
+    System.out.println(" \\(__)|");
+
+  }
+
+}
