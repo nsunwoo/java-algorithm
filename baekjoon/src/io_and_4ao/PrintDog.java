78893d11bf2f4a5217a5b3e6f45e96a3d7d33d14
nsunwoo <sunwoo.stats@gmail.com>
2023-02-08 12:07:57 +0900

    BOJ 10172: PrintDog

---
+++ baekjoon/src/io_and_4ao/PrintDog.java
@@ -0,0 +1,17 @@
+// https://www.acmicpc.net/problem/10172
+
+package io_and_4ao;
+
+public class PrintDog {
+
+  public static void main(String[] args) {
+    
+    System.out.println("|\\_/|");
+    System.out.println("|q p|   /}");
+    System.out.println("( 0 )\"\"\"\\");
+    System.out.println("|\"^\"`    |");
+    System.out.println("||_/=\\\\__|");
+
+  }
+
+}
