47eccd100d209dbe9f39a1d040be3720e9e7f33f
nsunwoo <sunwoo.stats@gmail.com>
2023-02-08 10:31:59 +0900

    BOJ 10998: Multiply

---
+++ baekjoon/src/io_and_4ao/Multiply.java
@@ -0,0 +1,23 @@
+// https://www.acmicpc.net/problem/10998
+
+package io_and_4ao;
+
+import java.util.Scanner;
+
+public class Multiply {
+
+  public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+    
+    int a, b;
+    a = sc.nextInt();
+    b = sc.nextInt();
+    
+    System.out.println(a * b);
+    
+    sc.close();
+
+  }
+
+}
