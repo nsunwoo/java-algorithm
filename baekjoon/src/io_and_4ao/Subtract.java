9c012cd9f109f80c8ba1e91464c323c9f92a2f62
nsunwoo <sunwoo.stats@gmail.com>
2023-02-08 10:28:23 +0900

    BOJ 1001: Subtract

---
+++ baekjoon/src/io_and_4ao/Subtract.java
@@ -0,0 +1,23 @@
+// https://www.acmicpc.net/problem/1001
+
+package io_and_4ao;
+
+import java.util.Scanner;
+
+public class Subtract {
+
+  public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+    
+    int a, b;
+    a = sc.nextInt();
+    b = sc.nextInt();
+    
+    System.out.println(a - b);
+    
+    sc.close();
+
+  }
+
+}
