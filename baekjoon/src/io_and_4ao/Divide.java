bac5dd1ec3e2529e6d85991f7892633eee8c1bc6
nsunwoo <sunwoo.stats@gmail.com>
2023-02-08 10:42:06 +0900

    BOJ 1008: Divide

---
+++ baekjoon/src/io_and_4ao/Divide.java
@@ -0,0 +1,23 @@
+// https://www.acmicpc.net/problem/1008
+
+package io_and_4ao;
+
+import java.util.Scanner;
+
+public class Divide {
+
+  public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+    
+    double a, b;
+    a = sc.nextDouble();
+    b = sc.nextDouble();
+    
+    System.out.println(a / b);
+    
+    sc.close();
+
+  }
+
+}
