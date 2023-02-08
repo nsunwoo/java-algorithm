0eb76677dc7025eadd562a353e04319dff73f5c7
nsunwoo <sunwoo.stats@gmail.com>
2023-02-08 11:13:09 +0900

    BOJ 18108: YearConvert

---
+++ baekjoon/src/io_and_4ao/YearConvert.java
@@ -0,0 +1,21 @@
+// https://www.acmicpc.net/problem/18108
+
+package io_and_4ao;
+
+import java.util.Scanner;
+
+public class YearConvert {
+
+  public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+    
+    int y = sc.nextInt();
+    
+    System.out.println(y - 543);
+    
+    sc.close();
+
+  }
+
+}
