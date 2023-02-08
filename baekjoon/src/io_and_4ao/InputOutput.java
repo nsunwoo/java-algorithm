4aad7a46456a42b322bf598d88e7c336bbdaae37
nsunwoo <sunwoo.stats@gmail.com>
2023-02-08 11:06:30 +0900

    BOJ 10926: InputOutput

---
+++ baekjoon/src/io_and_4ao/InputOutput.java
@@ -0,0 +1,22 @@
+// https://www.acmicpc.net/problem/10926
+
+package io_and_4ao;
+
+import java.util.Scanner;
+
+public class InputOutput {
+
+  public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+    
+    String id;
+    id = sc.nextLine();
+    
+    System.out.println(id + "??!");
+    
+    sc.close();
+
+  }
+
+}
