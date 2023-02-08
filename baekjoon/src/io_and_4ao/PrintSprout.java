98a9ff140d67d53e1f75b5d30480ce9260dd7f88
nsunwoo <sunwoo.stats@gmail.com>
2023-02-08 12:12:11 +0900

    BOJ 25083: PrintSprout

---
+++ baekjoon/src/io_and_4ao/PrintSprout.java
@@ -0,0 +1,18 @@
+// https://www.acmicpc.net/problem/25083
+
+package io_and_4ao;
+
+public class PrintSprout {
+
+  public static void main(String[] args) {
+    
+    System.out.println("         ,r'\"7");
+    System.out.println("r`-_   ,'  ,/");
+    System.out.println(" \\. \". L_r'");
+    System.out.println("   `~\\/");
+    System.out.println("      |");
+    System.out.println("      |");
+
+  }
+
+}
