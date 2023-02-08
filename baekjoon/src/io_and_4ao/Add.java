a46d9b1115c132203b2c250ad3fde02fed7c81aa
nsunwoo <sunwoo.stats@gmail.com>
2023-02-08 10:22:01 +0900

    fix: Close Scanner

--- baekjoon/src/io_and_4ao/Add.java
+++ baekjoon/src/io_and_4ao/Add.java
@@ -7,9 +7,13 @@ import java.util.Scanner;
 public class Add {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    
     int a, b;
     a = sc.nextInt();
     b = sc.nextInt();
+    
     System.out.println(a + b);
+    
+    sc.close();
   }
 }
