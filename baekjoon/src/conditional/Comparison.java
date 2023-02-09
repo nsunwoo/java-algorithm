9e8ed4862549e42e2cb233743a5163256963ee17
nsunwoo <sunwoo.stats@gmail.com>
2023-02-09 20:27:03 +0900

    BOJ 1330: Comparison

---
+++ baekjoon/src/conditional/Comparison.java
@@ -0,0 +1,29 @@
+// https://www.acmicpc.net/problem/1330
+
+package conditional;
+
+import java.util.Scanner;
+
+public class Comparison {
+
+  public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+    
+    int a, b;
+    a = sc.nextInt();
+    b = sc.nextInt();
+    
+    if (a > b) {
+      System.out.println(">");
+    } else if (a < b) {
+      System.out.println("<");
+    } else {
+      System.out.println("==");
+    }
+    
+    sc.close();
+
+  }
+
+}
