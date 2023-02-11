88bb7cbd20f4a06cd4bf37b9b180ff8681ff34e9
nsunwoo <sunwoo.stats@gmail.com>
2023-02-11 17:38:02 +0900

    BOJ 14681: Quadrant

---
+++ baekjoon/src/conditional/Quadrant.java
@@ -0,0 +1,24 @@
+//https://www.acmicpc.net/problem/14681
+
+package conditional;
+
+import java.util.Scanner;
+
+public class Quadrant {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int x, y;
+    x = sc.nextInt();
+    y = sc.nextInt();
+    if (x > 0 && y > 0) {
+      System.out.println(1);
+    } else if (x < 0 && y > 0) {
+      System.out.println(2);
+    } else if (x < 0 && y < 0) {
+      System.out.println(3);
+    } else {
+      System.out.println(4);
+    }
+    sc.close();
+  }
+}
