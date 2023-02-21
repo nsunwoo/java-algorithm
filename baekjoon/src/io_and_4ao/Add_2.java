18bdf54818f443e354037a274afd25a9d6046f9e
nsunwoo <sunwoo.stats@gmail.com>
2023-02-21 18:06:28 +0900

    BOJ 11382: Add_2

---
+++ baekjoon/src/io_and_4ao/Add_2.java
@@ -0,0 +1,18 @@
+//https://www.acmicpc.net/problem/11382
+
+package io_and_4ao;
+
+import java.util.Scanner;
+
+public class Add_2 {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    long a, b, c;
+    a = sc.nextLong();
+    b = sc.nextLong();
+    c = sc.nextLong();
+    System.out.println(a + b + c);
+    sc.close();
+  }
+}
